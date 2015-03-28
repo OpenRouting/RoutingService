e9e3c8884e1f84b555624f87d414d4ecdb53441e
christopherfricke <christopher.fricke@bmoregeo.com>
2015-03-28 08:14:15 -0400

    mock out controller factory

---
+++ controllers/factory.js
@@ -0,0 +1,15 @@
+/**
+ * Created by christopherfricke on 3/28/15.
+ */
+var ModelFactory = require('../models/factory').ModelFactory,
+    DocumentationController = require('./documentation').DocumentationController;
+
+exports.ControllerFactory = function(modelFactory) {
+    "use strict";
+    this.modelFactory = modelFactory || new ModelFactory();
+};
+
+exports.ControllerFactory.prototype.getDocumentationController = function(){
+    "use strict";
+    return new DocumentationController();
+};
