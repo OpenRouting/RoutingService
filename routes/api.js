c4d95158762abce24cdf375458001cc36f4a4f09
christopherfricke <christopher.fricke@bmoregeo.com>
2015-03-28 07:15:48 -0400

    add api routes

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,21 @@
 /**
  * Created by christopherfricke on 3/28/15.
  */
-var express = require('express');
+var express = require('express'),
+    tableHandler = require('../handlers/table').tableHandler,
+    listTableHandler = require('../handlers/table').listTableHandler,
+    routeHandler = require('../handlers/route').routeHandler,
+    directionHandler = require('../handlers/direction').directionHandler,
+    indexHandler = require('../handlers/index').indexHandler;
+
 var router = express.Router();
 
 /* GET home page. */
 
-router.get('/', function(req, res) {
-    res.render('route', { title: 'Express' });
-    res.render('direction', { title: 'Express' });
-    res.render('table', { title: 'Express' });
-});
+router.get('/', indexHandler);
+router.get('/route', routeHandler);
+router.get('/direction', directionHandler);
+router.get('/table', listTableHandler);
+router.get('/table/:table', tableHandler);
 
 module.exports = router;
