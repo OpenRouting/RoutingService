08cedbaea1b41649d06466d6150d3170d4d53e65
christopherfricke <christopher.fricke@bmoregeo.com>
2015-04-11 08:35:33 -0400

    sql scripts

---
+++ sql/get_waypoint.sql
@@ -0,0 +1,13 @@
+﻿-- Function: routing.get_waypoint(geometry)
+
+-- DROP FUNCTION routing.get_waypoint(geometry);
+
+CREATE OR REPLACE FUNCTION routing.get_waypoint(test_geom geometry)
+  RETURNS integer AS
+$BODY$
+    SELECT id::integer FROM routing.way_vertices_pgr ORDER BY the_geom <-> test_geom LIMIT 1;
+$BODY$
+  LANGUAGE sql VOLATILE
+  COST 100;
+ALTER FUNCTION routing.get_waypoint(geometry)
+  OWNER TO routing;
